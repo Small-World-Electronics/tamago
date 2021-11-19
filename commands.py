71b349b70965ac45a452b4e092a7c69804431696
beserge <bensergentanis@gmail.com>
2021-11-18 20:10:48 -0500

    Move commands out to seperate file

---
+++ commands.py
@@ -0,0 +1,130 @@
+stack = []
+memory = [0 for i in range(256)]
+
+def POP():
+    if(stack == []):
+        return # short circuit if stack is empty
+    a = stack.pop()
+    return a
+
+# only used internally for now, in code it's just #100 for example
+def PUSH(i):
+    stack.append(i)
+
+def ADD():
+    if(len(stack) < 2):
+        return
+    b = POP()
+    a = POP()
+    PUSH(a + b)
+
+def SUB():
+    if(len(stack) < 2):
+        return
+    b = POP()
+    a = POP()
+    PUSH(a - b)
+
+def MUL():
+    if(len(stack) < 2):
+        return
+    b = POP()
+    a = POP()
+    PUSH(a * b)
+
+def DIV():
+    if(len(stack) < 2):
+        return
+    b = POP()
+    a = POP()
+
+    # short circuit on divide by 0
+    if(b == 0):
+        PUSH(a)
+        PUSH(b)
+        return
+    
+    PUSH(a // b)
+
+def SHL():
+    if(len(stack) < 2):
+        return
+    b = POP()
+    a = POP()
+    a = a << b
+    PUSH(a)
+
+def SHR():
+    if(len(stack) < 2):
+        return
+    b = POP()
+    a = POP()
+    a = a >> b
+    PUSH(a)
+
+def MOD():
+    if(len(stack) < 2):
+        return
+    b = POP()
+    a = POP()
+    
+    # short circuit on divide by 0
+    if(b == 0):
+        PUSH(a)
+        PUSH(b)
+        return
+
+    a = a % b
+    PUSH(a)
+
+def DUP():
+    if(len(stack) < 1):
+        return
+    a = POP()
+    PUSH(a)
+    PUSH(a)
+
+def PRINT():
+    print(stack)
+
+def BPM():
+    if(len(stack) < 1):
+        return
+    a = POP()
+    global delay
+    delay = 60.0 / a
+
+def MIDION():
+    if(len(stack) < 1):
+        return
+    a = POP()
+    controller.note_on(a, velocity = 100)
+
+def MIDIOFF():
+    if(len(stack) < 1):
+        return
+    a = POP()
+    controller.note_off(a)
+
+def SWP():
+    if(len(stack) < 2):
+        return
+    a = POP()
+    b = POP()
+    PUSH(a)
+    PUSH(b)
+
+def STA():
+    if(len(stack) < 2):
+        return
+    add = POP()
+    val = POP()
+    if(add < 0 or add > 255):
+        return # only allowing 255 vars for now
+    memory[add] = val
+
+def LDA():
+    if(len(stack) < 1):
+        return
+    add = POP()
+    PUSH(memory[add])
--- stack_proto.py
+++ stack_proto.py
@@ -1,150 +1,19 @@
 import pygame.midi as midi
 import re
 import time
-import os
-import win32file
-import msvcrt
 from tkinter import *
+import commands
 
-ws = None
-text_box = None
-stack = []
+tk = None
+prog_box = None
+macro_box = None
 controller = None
 prog = []
-memory = [0 for i in range(256)]
 
 filename = 'prog.txt'
 
 delay = .5
 
-def POP():
-    if(stack == []):
-        return # short circuit if stack is empty
-    a = stack.pop()
-    return a
-
-# only used internally for now, in code it's just #100 for example
-def PUSH(i):
-    stack.append(i)
-
-def ADD():
-    if(len(stack) < 2):
-        return
-    b = POP()
-    a = POP()
-    PUSH(a + b)
-
-def SUB():
-    if(len(stack) < 2):
-        return
-    b = POP()
-    a = POP()
-    PUSH(a - b)
-
-def MUL():
-    if(len(stack) < 2):
-        return
-    b = POP()
-    a = POP()
-    PUSH(a * b)
-
-def DIV():
-    if(len(stack) < 2):
-        return
-    b = POP()
-    a = POP()
-
-    # short circuit on divide by 0
-    if(b == 0):
-        PUSH(a)
-        PUSH(b)
-        return
-    
-    PUSH(a // b)
-
-def SHL():
-    if(len(stack) < 2):
-        return
-    b = POP()
-    a = POP()
-    a = a << b
-    PUSH(a)
-
-def SHR():
-    if(len(stack) < 2):
-        return
-    b = POP()
-    a = POP()
-    a = a >> b
-    PUSH(a)
-
-def MOD():
-    if(len(stack) < 2):
-        return
-    b = POP()
-    a = POP()
-    
-    # short circuit on divide by 0
-    if(b == 0):
-        PUSH(a)
-        PUSH(b)
-        return
-
-    a = a % b
-    PUSH(a)
-
-def DUP():
-    if(len(stack) < 1):
-        return
-    a = POP()
-    PUSH(a)
-    PUSH(a)
-
-def PRINT():
-    print(stack)
-
-def BPM():
-    if(len(stack) < 1):
-        return
-    a = POP()
-    global delay
-    delay = 60.0 / a
-
-def MIDION():
-    if(len(stack) < 1):
-        return
-    a = POP()
-    controller.note_on(a, velocity = 100)
-
-def MIDIOFF():
-    if(len(stack) < 1):
-        return
-    a = POP()
-    controller.note_off(a)
-
-def SWP():
-    if(len(stack) < 2):
-        return
-    a = POP()
-    b = POP()
-    PUSH(a)
-    PUSH(b)
-
-def STA():
-    if(len(stack) < 2):
-        return
-    add = POP()
-    val = POP()
-    if(add < 0 or add > 255):
-        return # only allowing 255 vars for now
-    memory[add] = val
-
-def LDA():
-    if(len(stack) < 1):
-        return
-    add = POP()
-    PUSH(memory[add])
-    
 def midiInit():
     midi.init()
 
@@ -153,26 +22,27 @@ def midiInit():
     controller = midi.Output(3)
 
 def graphicsInit():
-    global ws
-    global text_box
-    ws = Tk()
-    ws.geometry('700x700')
-    text_box = Text(ws,height=50,width=100)
-    text_box.pack(expand=True)
-    
+    global tk, prog_box, text_box
+    tk = Tk()
+    tk.geometry('700x700')
+    macro_box = Text(tk, height=10, width = 100)
+    macro_box.pack(expand=True)
+    prog_box = Text(tk,height=20,width=100)
+    prog_box.pack(expand=True)
+
 def midiClose():
     controller.close()
     midi.quit()
 
-mapping = {"POP": POP, "PUSH": PUSH, "ADD": ADD, "SUB": SUB,
-           "MUL": MUL, "DIV": DIV, "SHL": SHL, "SHR": SHR,
-           "MOD": MOD, "PRINT": PRINT, "DUP": DUP, "MIDION": MIDION,
-           "MIDIOFF": MIDIOFF, "BPM": BPM, "SWP": SWP, "STA": STA,
-           "LDA": LDA}
+mapping = {"POP": commands.POP, "PUSH": commands.PUSH, "ADD": commands.ADD, "SUB": commands.SUB,
+           "MUL": commands.MUL, "DIV": commands.DIV, "SHL": commands.SHL, "SHR": commands.SHR,
+           "MOD": commands.MOD, "PRINT": commands.PRINT, "DUP": commands.DUP, "MIDION": commands.MIDION,
+           "MIDIOFF": commands.MIDIOFF, "BPM": commands.BPM, "SWP": commands.SWP, "STA": commands.STA,
+           "LDA": commands.LDA}
 
 def Parse():
     global prog
-    data = text_box.get("1.0",END)
+    data = prog_box.get("1.0",END)
     
     # split into lines then tokenize
     lines = data.splitlines()
@@ -187,21 +57,21 @@ def Parse():
 
     # match tokens with commands from dict or data
     for i in lines:
-        subprog = []
+        commands.SUBprog = []
         for j in range(len(i)):
             item = i[j]
             if(item == ''):
                 return # short citcuit on empty item
             elif(item[0] == '#'):
                 try:
-                    subprog.append(int(item[1:], 16))
+                    commands.SUBprog.append(int(item[1:], 16))
                 except:
                     continue
             elif item in mapping:
-                subprog.append(mapping[item])                
+                commands.SUBprog.append(mapping[item])                
             else:
                 return # short circuit on bad command name
-        temp_prog.append(subprog)
+        temp_prog.append(commands.SUBprog)
     prog = temp_prog.copy()
     
 def Init():
@@ -212,22 +82,21 @@ def ExecuteLine(linenum):
     global prog
     for command in prog[linenum]:
         if(type(command) is int):
-            PUSH(command)
+            commands.PUSH(command)
         else:
             command()
 
 now = time.time()
 def Run():    
-    global prog
-    global now
+    global prog, now
 
     prog = []
     commands = []
     linenum = 0
     while(True):
         Parse()
-        ws.update_idletasks()
-        ws.update()
+        tk.update_idletasks()
+        tk.update()
 
         nownow = time.time()
         if(nownow - now >= delay):
@@ -239,3 +108,5 @@ def Run():
 def main():
     Init()
     Run()
+
+main()
